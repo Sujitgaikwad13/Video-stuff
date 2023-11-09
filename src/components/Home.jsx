01b92c5aa6df990908b988929d09acc2eda3af64
Sujitgaikwad13 <sujitgaikwad121@gmail.com>
2023-11-09 20:50:14 +0530

    change the video ratio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -76,25 +76,25 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w="full" h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watching the future unfold before our eyes
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img2} />
+      <Image src={img2} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Gaming: Where the future meets fun
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Level up and conquer the gaming world
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Embrace the night, live the life
       </Heading>
